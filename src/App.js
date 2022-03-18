602cccddd1362e14580a30ad08b54fffbb905937
Tino Muzambi <tinomuzambi@gmail.com>
2022-03-18 13:21:48 +0200

    Moves footer to component.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 
 const App = () => {
@@ -147,41 +148,7 @@ const App = () => {
 				</div>
 			</main>
 
-			<footer className="scene">
-				<article className="content">
-					<div id="socialmedia">
-						<ul className="group">
-							<li>
-								<a href="https://twitter.com">
-									<img
-										className="icon"
-										src="https://landonhotel.com/images/socialmedia/twitter.png"
-										alt="icon for twitter"
-									/>
-								</a>
-							</li>
-							<li>
-								<a href="http://www.facebook.com">
-									<img
-										className="icon"
-										src="https://landonhotel.com/images/socialmedia/facebook.png"
-										alt="icon for facebook"
-									/>
-								</a>
-							</li>
-							<li>
-								<a href="http://www.youtube.com">
-									<img
-										className="icon"
-										src="https://landonhotel.com/images/socialmedia/youtube.png"
-										alt="icon for youtube"
-									/>
-								</a>
-							</li>
-						</ul>
-					</div>
-				</article>
-			</footer>
+			<Footer />
 		</div>
 	);
 };
