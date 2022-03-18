1973d4e33b3f4b7680c488493c7027cb84e05bff
Tino Muzambi <tinomuzambi@gmail.com>
2022-03-18 13:37:58 +0200

    Maps through gallery data.

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,29 +1,18 @@
+import galleryData from "./data/gallery.json";
+
 const Welcome = () => {
 	return (
 		<div className="scene" id="welcome">
 			<article className="content">
 				<div className="gallery">
-					<img
-						src="https://landonhotel.com/images/hotel/intro_room.jpg"
-						alt="Intro Gallery Room Sample Pictures"
-					/>
-					<img
-						src="https://landonhotel.com/images/hotel/intro_pool.jpg"
-						alt="Intro Gallery Pool Sample Pictures"
-					/>
-					<img
-						src="https://landonhotel.com/images/hotel/intro_dining.jpg"
-						alt="Intro Gallery Dining Sample Pictures"
-					/>
-					<img
-						src="https://landonhotel.com/images/hotel/intro_attractions.jpg"
-						alt="Intro Gallery Attractions Sample Pictures"
-					/>
-					<img
-						className="hidesm"
-						src="https://landonhotel.com/images/hotel/intro_wedding.jpg"
-						alt="Intro Gallery Dining Sample Pictures"
-					/>
+					{galleryData.map((item, key) => (
+						<img
+							src={item.src}
+							alt={item.alt}
+							className={item.class}
+							key={key}
+						/>
+					))}
 				</div>
 				<h1>Welcome to the Landon&nbsp;Hotel</h1>
 				<p>
