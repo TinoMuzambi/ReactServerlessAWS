ddbc24239553187006c5014d3ee605e80de3945b
Tino Muzambi <tinomuzambi@gmail.com>
2022-03-18 13:49:28 +0200

    Maps through services.

--- src/components/HotelInfo.jsx
+++ src/components/HotelInfo.jsx
@@ -1,3 +1,5 @@
+import servicesAmenitiesData from "./data/services_amenities.json";
+
 const HotelInfo = () => {
 	return (
 		<div className="scene" id="hotelinfo">
@@ -41,21 +43,9 @@ const HotelInfo = () => {
 						your stay comfortable, and your experience one-of-a-kind.
 					</p>
 					<ul>
-						<li>Indoor pool</li>
-						<li>24-hour fitness center</li>
-						<li>Massage therapy</li>
-						<li>Full service spa</li>
-						<li>In-room jacuzzi tubs</li>
-						<li>Rooftop café &amp; smoothie bar</li>
-						<li>Coffee bar &amp; pastry shop</li>
-						<li>Traditional continental breakfast</li>
-						<li>24-hour concierge service</li>
-						<li>Business center</li>
-						<li>Complimentary wireless service</li>
-						<li>Laundry &amp; dry cleaning service</li>
-						<li>Daily paper</li>
-						<li>Certified "green" hotel</li>
-						<li>Pet-friendly rooms &amp; common areas</li>
+						{servicesAmenitiesData.map((item, key) => (
+							<li key={key}>{item.name}</li>
+						))}
 					</ul>
 				</section>
 				<section className="checklist" id="accessibility">
